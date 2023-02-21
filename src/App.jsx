eca583641b1a1ca8410a088b49b915a1a6c1f1f2
IsmaelHernandez <ismael.hernandez@upam.edu.mx>
2023-02-21 12:56:25 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>hola isma</h1>
+    </div>
+  )
+}
+
+export default App
